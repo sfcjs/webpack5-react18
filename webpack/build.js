486a7d4227f8021372e747676fff6951717d635d
haha <haha@haha.haha>
2022-03-03 21:05:46 +0800

    打包拷贝public

--- webpack/build.js
+++ webpack/build.js
@@ -1,11 +1,17 @@
 const webpack = require("webpack");
 const configFactory = require("./webpack.config");
+const fs = require("fs-extra");
+const paths = require("./paths");
 
 process.env.NODE_ENV = "production";
 
 const config = configFactory("production");
 webpack(config).run((err, stats) => {
   if (!err) {
+    fs.copySync(paths.appPublic, paths.appBuild, {
+      dereference: true,
+      filter: (file) => file !== paths.appHtml,
+    });
     console.log(">>> 打包成功");
   } else {
     console.log(">>> 打包失败");
---
+++ webpack/paths.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const fs = require("fs");
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath);
+
+const moduleFileExtensions = ["web.mjs", "mjs", "web.js", "js", "web.ts", "ts", "web.tsx", "tsx", "json", "web.jsx", "jsx"];
+const buildPath = process.env.BUILD_PATH || "build";
+
+const resolveModule = (resolveFn, filePath) => {
+  const extension = moduleFileExtensions.find((extension) => fs.existsSync(resolveFn(`${filePath}.${extension}`)));
+
+  if (extension) {
+    return resolveFn(`${filePath}.${extension}`);
+  }
+
+  return resolveFn(`${filePath}.js`);
+};
+
+module.exports = {
+  dotenv: resolveApp(".env"),
+  appPath: resolveApp("."),
+  appBuild: resolveApp(buildPath),
+  appPublic: resolveApp("public"),
+  appHtml: resolveApp("public/index.html"),
+  appIndexJs: resolveModule(resolveApp, "src/index"),
+  appPackageJson: resolveApp("package.json"),
+  appSrc: resolveApp("src"),
+  appTsConfig: resolveApp("tsconfig.json"),
+  appJsConfig: resolveApp("jsconfig.json"),
+  testsSetup: resolveModule(resolveApp, "src/setupTests"),
+  proxySetup: resolveApp("src/setupProxy.js"),
+  appNodeModules: resolveApp("node_modules"),
+};
